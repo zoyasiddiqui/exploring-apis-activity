c249814f28ef81ed66407f7436bbd6bfb403f22e
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-09-27 19:27:36 -0400

    add variables for api requests

--- chatgpt_api.java
+++ chatgpt_api.java
@@ -1,2 +1,11 @@
 public class chatgpt_api {
+
+    private static final String API_URL = "https://api.openai.com/v1/chat/completions";
+    private static final String API_TOKEN = System.getenv("API_TOKEN");
+
+    public static String getApiToken() {
+        return API_TOKEN;
+    }
+
+
 }

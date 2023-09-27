8a76603a0eb657f163be405c4276dc8821e77008
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-09-27 19:51:07 -0400

    added necessary code but haven't fixed import issues

--- chatgpt_api.java
+++ chatgpt_api.java
@@ -1,3 +1,8 @@
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class chatgpt_api {
 
     private static final String API_URL = "https://api.openai.com/v1/chat/completions";
@@ -5,6 +10,35 @@ public class chatgpt_api {
 
     public static String getApiToken() {
         return API_TOKEN;
+    }
+
+    public JSONObject getMessage (String[] messages) {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("model", "gpt-3.5-turbo");
+        requestBody.put("messages", messages);
+        RequestBody body = RequestBody.create(mediaType, requestBody.toString());
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .method("POST", body)
+                .addHeader("Authorization", API_TOKEN)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt("status_code") == 200) {
+                return responseBody.getJSONObject("choices");
+            } else {
+                throw new RuntimeException(responseBody.getString("message"));
+            }
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(String.valueOf(e));
+        }
     }
 
 
